6cf1c42a970b1a276f795b50551b380463c10080
Agata Wojcik <agataizabelawojcik@gmail.com>
2020-08-13 20:59:11 +0200

    rails-longest-word-game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,9 @@
 class GamesController < ApplicationController
   def new
     # raise
-    @letters = ('a'..'z').to_a
+    @letters = ('a'..'z').to_a.sample(10)
+    # @letters.map { ('a'..'z').to_a[rand(26)] }.join
+    #  @letters.sample(15)
     #create a new 
     #@letters instance variable storing random letters from the alphabet
     #display it in the view
