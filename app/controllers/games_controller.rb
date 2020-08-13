187e0e1855a62cefeb3b1d002e578becba524386
Agata Wojcik <agataizabelawojcik@gmail.com>
2020-08-13 19:38:39 +0200

    New game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,12 @@
+class GamesController < ApplicationController
+  def new
+    # raise
+    # @letters = *('a'..'z')
+    #create a new 
+    #@letters instance variable storing random letters from the alphabet
+    #display it in the view
+  end
+
+  def score
+  end
+end
