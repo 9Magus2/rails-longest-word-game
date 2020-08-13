f37902c781acea508a8a8517a0b1615ee429b07e
Agata Wojcik <agataizabelawojcik@gmail.com>
2020-08-13 20:23:59 +0200

    hidden

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,7 @@
 class GamesController < ApplicationController
   def new
     # raise
-    # @letters = *('a'..'z')
+    @letters = ('a'..'z').to_a
     #create a new 
     #@letters instance variable storing random letters from the alphabet
     #display it in the view
